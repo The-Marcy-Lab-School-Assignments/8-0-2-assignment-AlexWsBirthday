2a944321151e49e5dc64da45af00ba40e269b368
gonzalorportfolio <gromeroeducation@gmail.com>
2024-09-28 12:18:14 -0400

    feedback

--- giphy-search/src/App.jsx
+++ giphy-search/src/App.jsx
@@ -3,11 +3,13 @@ import GifContainer from './components/GifContainer'
 import GifSearch from './components/GifSearch'
 import { handleFetch } from './utils';
 import { useState, useEffect } from 'react';
-import API_KEY from './config';
+// import API_KEY from './config'; // I coomented this outwe no longer need this here because we are using the server.
 // import { API_KEY } from '../../server/';
 
-
-const URL = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`
+// We need to change this to reflect the server's route
+// const URL = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`
+// Remember that you set up the proxy in the vite.config.js file, this allows just to use the end point without the full URL
+const URL = `/api/gifs`;// I added this line to connect to the server
 
 function App() {
   const [gifs, setGifs] = useState([]);
